619725d0d36f8704dc8cb4a72b193cc36964acfa
Philip <philip@deranged.dk>
2023-06-27 20:47:32 +0200

    first take on list ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css';
+import {
+  List,
+  ListItem,
+  UnorderedList,
+} from '@chakra-ui/react'
 
 function App() {
   return (
     <div className='App'>
       <h1>Tory</h1>
+      <UnorderedList spacing={2}>
+        <ListItem>En ting</ListItem>
+        <ListItem>En ting 2</ListItem>
+        <ListItem>En ting mere</ListItem>
+      </UnorderedList>
     </div>
   );
 }
