6971e30e3d3e6d330eedd8834d22d478c594ac1d
Philip <philip@deranged.dk>
2023-06-27 21:05:45 +0200

    felt to crammed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import './App.css';
 import {
-  List,
   ListItem,
-  UnorderedList,
+  OrderedList,
+  Box
 } from '@chakra-ui/react'
 
 function App() {
   return (
-    <div className='App'>
+    <Box className='App' p={5}>
       <h1>Box 1</h1>
       <h2>Indholdsfortegnelse</h2>
-      <UnorderedList spacing={2}>
+      <OrderedList spacing={2}>
         <ListItem>En ting</ListItem>
         <ListItem>En ting 2</ListItem>
         <ListItem>En ting mere</ListItem>
-      </UnorderedList>
-    </div>
+      </OrderedList>
+    </Box>
   );
 }
 
