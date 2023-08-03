b2213f1508a9e48db1835fe934e82cc3c0f53c8b
Philip <philip@deranged.dk>
2023-08-03 20:54:10 +0200

    and this one should list of the inventory of a ccontainer

---
+++ src/ContainerInventory.tsx
@@ -0,0 +1,15 @@
+import {
+  ListItem,
+  OrderedList,
+} from '@chakra-ui/react';
+
+export function ContainerInventory({ items }: { items: Array<any>}) {
+  return (
+    <OrderedList spacing={2}>
+      { 
+        items.map((item: any)=> 
+          <ListItem key={item.id} value={item.name}>{ item.name }</ListItem>
+      )}
+    </OrderedList>
+  )
+}
