820f72652ec42979470a113aa8e0dfa108b15b06
Philip <philip@deranged.dk>
2023-08-03 20:53:40 +0200

    this file should contain.... a list of containers

--- src/ContainerList.tsx
+++ src/ContainerList.tsx
@@ -1,15 +1,5 @@
-import {
-  ListItem,
-  OrderedList,
-} from '@chakra-ui/react';
-
-export function ContainerList({ items }: { items: Array<any>}) {
+export function ContainerList() {
   return (
-    <OrderedList spacing={2}>
-      { 
-        items.map((item: any)=> 
-          <ListItem key={item.id} value={item.name}>{ item.name }</ListItem>
-      )}
-    </OrderedList>
+    <h1>Container List</h1>
   )
 }
